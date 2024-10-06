9f50eee3881b9162831fe862096f91aba88d52ee
Carlityn <carloscaballero021021@gmail.com>
2024-10-06 13:42:48 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>MATHISA</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <form action="">
+            <h1>Iniciar Sesion</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Usuario" required>
+                <i class='bx bx-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Contraseña" required>
+                <i class='bx bx-lock-alt'></i>
+            </div>
+
+            <button type="submit" class="btn">Aceptar</button>
+
+            <script src="script.js"></script>
+            <!-- This HTML document represents a basic webpage structure.
+It includes a header with a title, a navigation menu with links, a main content section with articles, and a footer with contact information. -->
+
+        </form>
+    </div>
+</body>
+
+</html>
